a34b9eac6cd4c006d4284943485c3131a16e3aed
Rafael Cruz <rcruz@mundipagg.com>
2016-08-09 13:51:50 -0300

    RabbitMQ Implementation

---
+++ RabbitMQ/Consumer/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = "192.168.8.194";
+            string userName = "sys";
+            string password = "123456";
+            string queueName = "Message-Queue";
+
+            const int NUMBER_OF_WORKROLES = 3;
+
+            //Cria a conexão com o RabbitMq
+            var factory = new ConnectionFactory()
+            {
+                HostName = hostName,
+                UserName = userName,
+                Password = password,
+            };
+
+            //Cria a conexão
+            IConnection connection = factory.CreateConnection();
+
+            //cria a canal de comunicação com a rabbit mq
+            IModel channel = connection.CreateModel();
+
+            for (int i = 0; i < NUMBER_OF_WORKROLES; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    lock (channel)
+                    {
+
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.ConsumerTag = Guid.NewGuid().ToString(); // Tag de identificação do consumidor no RabbitMQ
+                        consumer.Received += (sender, ea) =>
+                        {
+                            var body = ea.Body;
+                            var brokerMessage = Encoding.Default.GetString(ea.Body);
+
+                            Console.WriteLine($"Mensagem recebida com o valor: {brokerMessage}");
+                            
+                            //Diz ao RabbitMQ que a mensagem foi lida com sucesso pelo consumidor
+                            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: true);
+                        };
+
+                        //Registra os consumidor no RabbitMQ
+                        channel.BasicConsume(queueName, noAck: false, consumer: consumer);
+
+                    }
+                });
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ RabbitMQ/Publisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = "192.168.8.194";
+            string userName = "sys";
+            string password = "123456";
+            string queueName = "Message-Queue";
+
+            //Cria a conexão com o RabbitMq
+            var factory = new ConnectionFactory()
+            {
+                HostName = hostName,
+                UserName = userName,
+                Password = password,
+            };
+
+            //Cria a conexão
+            IConnection connection = factory.CreateConnection();
+
+            //cria a canal de comunicação com a rabbit mq
+            IModel channel = connection.CreateModel();
+
+            //Cria a fila caso não exista
+            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            for (int i = 1; i <= 500; i++)
+            {
+                String mensagem = $"Enviando a mensagem {i}";
+                byte[] body = Encoding.Default.GetBytes(mensagem);
+
+                //Seta a mensagem como persistente
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                //Envia a mensagem para fila
+                channel.BasicPublish(exchange: String.Empty, routingKey: queueName, basicProperties: properties, body:body);
+            }
+
+            Console.WriteLine("Mensagem enviadas para fila");
+            Console.Read();            
+        }
+    }
+}
